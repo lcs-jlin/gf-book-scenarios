3831ac49ba50f39abdd23d2e150b597002b21674
Justy Lin <jlin@lcs.on.ca>
2019-04-10 11:51:38 -0400

    Got the program to say hello to every student on the list

--- chapter06/piano-1/Piano.java
+++ chapter06/piano-1/Piano.java
@@ -11,8 +11,14 @@ public class Piano extends World
     /**
      * * Instance variable (can ve used anywhere below)
      */
-     int frames;
-     
+    //primitive data type
+    int frames;
+    // Object data type... square brackets [] indicate an array ( a list)
+    String[] studentNames = { 
+            "Sara", "Chelsea", "Jack", "Maxwell", "Gavin" , 
+            "Minah", "Brandon", "Jeewoo", "Peter", "Justy" 
+        };
+
     /**
      * Make the piano.
      */
@@ -21,16 +27,28 @@ public class Piano extends World
         super(800, 340, 1);
         // Star tracking frame
         frames = 0;
+        // Initialize the array of student nmaes
+
     }
+
     /**
      * * Act - called repeatably to create animation
      */
     public void act()
     {
+        // Every second say hello to the nest person in the list
+        if (frames % 60 == 0)
+        {
+            if ( frames / 60 < 10 )
+            {
+            showText("Hello " + studentNames [ frames / 60 ], 400, 170);
+        }
+        }
         // Keep track of the frames 
         frames += 1;
-        
+
         // Show correct frame
         showText("" + frames, 100, 100);
+        
     }
 }
