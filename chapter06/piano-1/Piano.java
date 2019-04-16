34b6457153134d4af5e0632e45f9673be5aec2ca
Justy Lin <jlin@lcs.on.ca>
2019-04-16 11:37:10 -0400

    Last fix for 4/16

--- chapter06/piano-1/Piano.java
+++ chapter06/piano-1/Piano.java
@@ -19,11 +19,11 @@ public class Piano extends World
             "Minah", "Brandon", "Jeewoo", "Peter", "Justy" 
         };
 
-        // Track the white keys and notes
-        String[] whiteKeys = {"a", "s", "d", "f", "g", "h", "j", "k", "l",
+    // Track the white keys and notes
+    String[] whiteKeys = {"a", "s", "d", "f", "g", "h", "j", "k", "l",
             "i", "'", "\\"};
-            String[] whiteNotes = {"3c", "3d", "3e", "3f", "3g", "3a", "3b", "4c",
-            "4e", "4f", "4g"};
+    String[] whiteNotes = {"3c", "3d", "3e", "3f", "3g", "3a", "3b", "4c",
+            "4d", "4e", "4f", "4g"};
     /**
      * Make the piano.
      */
@@ -45,12 +45,30 @@ public class Piano extends World
         // Every second say hello to the nest person in the list
         // Use a Boolean AND to track both conditions
         // The shwotext statement only runs when BOTH conditions are true
-        //NOTE:white
+        //NOTE:whiteKeys.length automatically returns the correct number
+        // of values in the array (so that we won't run past the end)
+        int position = frames / 60;
         if ((frames % 60 == 0) && (frames % 60 < whiteKeys.length)) 
         {
+            // Assemble the piano by creating each key one by one
+            // This line creates the key object
+            Key anotherKey = new Key(whiteKeys[frames / 60], whiteNotes[frames / 60]);
+
+            // This line actually adds the object to the Piano world
+            // OBJECT HORIZONTAL POSITION WERTICAL POSITION
+            // NOTE: Check phone to see sketch of logic explaining the expression
+            //  for the x position (horizontal position) of each key
+            addObject(anotherKey, 54 + position * 63, 140);
+
+            // Only show a message when we are in the bounds of the array
+            showText("Array index is:" + frames / 60, 400, 250);
+
+            // Only say hello when we are in the bounds of the array 
+            // Say hello to everyone in the class!
+            // shoeText("Hello" + peopleInClass[frames/ 60], 400, 170);
             if ( frames / 60 < 10 )
             {
-                showText("Hello " + studentNames [ frames / 60 ], 400, 170);
+                //showText("Hello " + peopleInClass [ frames / 60 ], 400, 170);
             }
         }
         // Keep track of the frames 
@@ -67,7 +85,6 @@ public class Piano extends World
      */
     private void prepare()
     {
-        
-        
+
     }
 }
